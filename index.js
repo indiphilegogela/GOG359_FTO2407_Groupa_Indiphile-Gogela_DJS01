a38f49d6c4b2f33a6fec7da896671688b01f4eba
indiphilegogela <156888615+indiphilegogela@users.noreply.github.com>
2025-04-27 17:42:27 +0200

    Update index.js
    Did the function to create new velocity

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
 // Given Parameters
 const vel = 10000; // velocity (km/h)
 const acc = 3; // acceleration (m/s^2)
-const time = 3600; // seconds (1 hour)
+const time = 3600; // time (1 hour)
 const d = 0; // distance (km)
 const fuel = 5000; // remaining fuel (kg)
 const fbr = 0.5; // fuel burn rate (kg/s)
@@ -16,7 +16,10 @@ const fbr = 0.5; // fuel burn rate (kg/s)
 
 const d2 = d + (vel*time) //calcultes new distance
 const rf = fbr*time //calculates remaining fuel
-const vel2 = calcNewVel(acc, vel, time) //calculates new velocity based on acceleration
+
+
+//Function to Calculate New Velocity
+const vel2 = calcNewVel(vel, acc, time);
 
 // Pick up an error with how the function below is called and make it robust to such errors
 calcNewVel = (vel, acc, time) => { 
